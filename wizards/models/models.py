13312b9072c259bd191e62d266cbc43f3c13cf13
xxjcaxx <jocasal@gmail.com>
2019-01-21 09:15:06 +0000

    wizards teatres

--- wizards/models/models.py
+++ wizards/models/models.py
@@ -54,4 +54,73 @@ class butaques(models.Model):
 
 
 
+class w_reserves(models.TransientModel):
+     _name = 'wizards.w_reserves'
+ 
+     def _default_teatre(self):
+         return self.env['wizards.teatres'].browse(self._context.get('active_id')) # El context conté, entre altre coses, el active_id del model que està obert.
+ 
+     teatre = fields.Many2one('wizards.teatres',default=_default_teatre)
+     obra = fields.Many2one('wizards.obres')
+     actuacio = fields.Many2one('wizards.actuacions',required=True)
+     butaca = fields.Many2one('wizards.butaques',required=True)
+     state = fields.Selection([
+        ('teatre', "Teatre Selection"),
+        ('obra', "Obra Selection"),                                             
+        ('actuacio', "Actuacio Selection"),
+        ('butaca', "butaca Selection"),
+        ('fin', "Fin"),
+        ], default='teatre')
+ 
+ 
+     @api.onchange('teatre')
+     def _oc_teatre(self):
+        if len(self.teatre) > 0:
+         actuacions = self.env['wizards.actuacions'].search([('teatre','=',self.teatre.id)])
+         print(actuacions)
+         obres = actuacions.mapped('obra')
+         print(obres)
+         self.state='obra'
+         return { 'domain': {'obra': [('id', 'in', obres.ids)]},}
+
+     @api.onchange('obra')
+     def _oc_obra(self):
+        if len(self.obra) > 0:
+          actuacions = self.env['wizards.actuacions'].search([('teatre','=',self.teatre.id),('obra','=',self.obra.id)])
+
+          self.state='actuacio'
+          return { 'domain': {'actuacio': [('id', 'in', actuacions.ids)]},}
+
+
+     @api.onchange('actuacio')
+     def _oc_actuacio(self):
+        if len(self.actuacio) > 0:
+          print('butaques ******************************************')
+          butaques = self.env['wizards.butaques'].search([('teatre','=',self.actuacio.teatre.id)])
+          b_reservades = self.actuacio.reserves.mapped('butaca')
+          print(b_reservades)
+          b_disponibles = butaques - b_reservades
+          print(b_disponibles)
+
+          self.state='butaca'
+          return { 'domain': {'butaca': [('id', 'in', b_disponibles.ids)]},}
+
+     @api.onchange('butaca')
+     def _oc_butaca(self):
+        if len(self.butaca) > 0:
+            self.state='fin'
+
+     @api.multi
+     def reserva(self):
+         reserva = self.env['wizards.reserves'].create({'actuacio':self.actuacio.id,'butaca':self.butaca.id,'name':str(self.actuacio.name)+" - "+str(self.butaca.name)})
+         return {
+    'name': 'Reserves',
+    'view_type': 'form',
+    'view_mode': 'form',
+    'res_model': 'wizards.reserves',
+    'res_id': reserva.id,
+    'context': self._context,
+    'type': 'ir.actions.act_window',
+    'target': 'current',
+                 }
 
